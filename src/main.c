64b2ad8ed40e0260b1bcabbb15418edefb882d9c
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2025-04-04 11:35:58 -0300

    feat: watchlist

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include "watchlist.h"
 
 int main()
 {
-  printf("Hello, World!");
+  for (int i = 0; i < 9; i++)
+  {
+    printf("%s\n", watchlist[i]);
+  }
   return 0;
 }
---
+++ src/watchlist.h
@@ -0,0 +1,15 @@
+#ifndef WATCHLIST_H
+#define WATCHLIST_H
+
+static const char *watchlist[9] = {
+    "/etc/",
+    "/bin/",
+    "/usr/bin/",
+    "/sbin/",
+    "/usr/sbin",
+    "/lib/",
+    "/usr/lib/",
+    "/usr/include/",
+    "/boot/"};
+
+#endif
