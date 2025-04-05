37e81312650fbccb6da9899fb393b0657062c3e6
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2025-04-05 16:15:58 -0300

    feat: open each dir in list

--- src/main.c
+++ src/main.c
@@ -1,11 +1,24 @@
+#include <dirent.h>
 #include <stdio.h>
 #include "watchlist.h"
 
 int main()
 {
+  DIR *d;
+  struct dirent *dir;
+
   for (int i = 0; i < 9; i++)
   {
-    printf("%s\n", watchlist[i]);
+    d = opendir(watchlist[i]);
+    if (d)
+    {
+      while ((dir = readdir(d)) != NULL)
+      {
+        printf("%s\n", dir->d_name);
+      }
+      closedir(d);
+    }
   }
-  return 0;
+
+  return (0);
 }
