626f31ac6147f189ecfdeebaf3edf0003ba04760
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2025-04-10 10:55:53 -0300

    feat: hash files

--- src/main.c
+++ src/main.c
@@ -1,7 +1,54 @@
-#include <dirent.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
 #include "watchlist.h"
+
+#define BUFFER_SIZE 4096 // Page size (4KB)
+
+// Generate hash for a file
+void generate_checksum(const char *path)
+{
+  printf("%s (File)\n", path);
+  FILE *fp;
+  EVP_MD *md;
+  EVP_MD_CTX *mdctx;
+  unsigned char hash[EVP_MAX_MD_SIZE];
+  int hash_length;
+  unsigned char *buffer;
+  unsigned int n_bytes;
+
+  fp = fopen(path, "rb");
+  md = EVP_get_digestbyname("sha256");
+  mdctx = EVP_MD_CTX_new();
+  EVP_DigestInit_ex(mdctx, md, NULL);
+
+  if (buffer = malloc(BUFFER_SIZE))
+  {
+    while ((n_bytes = fread(buffer, 1, BUFFER_SIZE, fp)) > 0)
+    {
+      EVP_DigestUpdate(mdctx, buffer, n_bytes);
+    }
+
+    fclose(fp);
+    EVP_DigestFinal_ex(mdctx, hash, &hash_length);
+    free(buffer);
+    EVP_MD_CTX_free(mdctx);
+  }
+  else
+  {
+    printf("Something went wrong.\n");
+    return;
+  }
+
+  for (int i = 0; i < hash_length; ++i)
+  {
+    printf("%02x", hash[i]);
+  }
+
+  printf("\n");
+}
 
 // Recursive directory traversal
 void directory_traversal(const char *path)
@@ -15,22 +62,25 @@ void directory_traversal(const char *path)
   // Read directory
   while ((dirent = readdir(dir)) != NULL)
   {
+    char curr_path[PATH_MAX];
+    snprintf(curr_path, sizeof(curr_path), "%s/%s", path, dirent->d_name);
+
     if (dirent->d_type == DT_DIR)
     {
       // Skip '.' and '..'
       if (strcmp(dirent->d_name, ".") == 0 || strcmp(dirent->d_name, "..") == 0)
         continue;
 
-      printf("%s (Directory)\n", dirent->d_name);
+      // Generate hash
+      generate_checksum(curr_path);
 
       // Go inside this directory, list its contents as well
-      char next_path[PATH_MAX];
-      snprintf(next_path, sizeof(next_path), "%s/%s", path, dirent->d_name);
-      directory_traversal(next_path);
+      directory_traversal(curr_path);
     }
     else
     {
-      printf("%s (File)\n", dirent->d_name);
+      // Generate hash
+      generate_checksum(curr_path);
     }
   }
   closedir(dir);
@@ -38,19 +88,10 @@ void directory_traversal(const char *path)
 
 int main()
 {
-
-  // for (int i = 0; i < 9; i++)
-  //{
-  //   d = opendir(watchlist[i]);
-  //   if (d)
-  //   {
-  //     while ((dir = readdir(d)) != NULL)
-  //     {
-  //       printf("%s\n", dir->d_name);
-  //     }
-  //     closedir(d);
-  //   }
-  // }
+  for (int i = 0; i < 9; i++)
+  {
+    directory_traversal(watchlist[i]);
+  }
 
   return (0);
 }
