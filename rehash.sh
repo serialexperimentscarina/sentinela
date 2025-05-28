12fd08e4e9ae9aec093c376f45bc86c9d22ee3a3
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2025-05-28 19:41:06 -0300

    feat: more options on rehash function

--- rehash.sh
+++ rehash.sh
@@ -14,6 +14,27 @@ fi
 echo "stopping service..."
 sudo systemctl stop sentinela.service
 
+# prompt for copy of hash database
+while true; do
+    read -p "do you wish to create a backup copy of the current hash database? - copy will be saved under /var/backups/hashes_copy.json (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo cp /var/lib/sentinela/hashes.json /var/backups/hashes_copy.json; break;;
+        [Nn]* ) echo "skipping backup...."; break;;
+        * ) echo "please answer with 'y' or 'n'";;
+    esac
+done
+
+# prompt to clear logs
+while true; do
+    read -p "do you wish to clear current logs? (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo rm /var/log/sentinela.log; break;;
+        [Nn]* ) echo "skipping logs...."; break;;
+        * ) echo "please answer with 'y' or 'n'";;
+    esac
+done
+
+
 # remove sentinela files
 echo "removing current hashfile..."
 sudo rm -r /var/lib/sentinela
